af5efccbc3e04bb0cbb3103b037e998b1be87e9d
Milos Subak <subakmil@gmail.com>
2021-02-11 14:12:54 +0100

    Projects before styling

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-export const reactProjects = [
+const reactProjects = [
   {
     name: "Jammming",
     description:
--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-import { reactProjects } from "./data";
+// Data definition
+const reactProjects = [
+  {
+    name: "Jammming",
+    description:
+      "Website that allows users to search the Spotify library, create a custom playlist and save it to their Spotify account.",
+    img: "./img/jammming.png",
+    url: "https://thirsty-elion-d32e4e.netlify.app/",
+  },
+  {
+    name: "Passing Thoughts",
+    description:
+      "Place for our passing thoughts. Once you add a short thought, it’ll disappear after just 15 seconds.",
+    img: "./img/passing.png",
+    url: "https://stoic-yonath-e8d4fb.netlify.app/",
+  },
+  {
+    name: "Color Generator",
+    description:
+      "Tool that will return multiple tints and shades of the specified color (in hex format). Based on John Smilga's tutorial.",
+    img: "./img/color-mixer.png",
+    url: "https://color-generator-subakmil.netlify.app/",
+  },
+  {
+    name: "Tours",
+    description:
+      "Website built in React that displays multiple tours recommendations. Based on John Smilga's tutorial.",
+    img: "./img/tours.png",
+    url: "https://tours-subakmil.netlify.app/",
+  },
+  {
+    name: "Copycat",
+    description:
+      "When users type into the textbox, a copy of the text will appear below the cat image, suggesting that the cat is being a copycat. Clicking on the image of the cat will toggle whether or not it is able to copy the user.",
+    img: "./img/copycat.png",
+    url: "https://copycat-subakmil.netlify.app/",
+  },
+];
 
 // Age auto-update
-const ageCalculator = () => {
+function ageCalculator() {
   const myBirthday = new Date("October 05, 92");
   const actualDate = new Date();
 
@@ -19,6 +56,21 @@ const ageCalculator = () => {
     age = yearDiff;
     return age;
   }
-};
+}
 
 document.getElementById("ageJs").innerHTML = ageCalculator();
+
+// Display React projects
+
+console.log(reactProjects);
+
+const listOfProjects = reactProjects.map((item) => {
+  return `<div>
+      <a href=${item.url} target="_blank">
+        <img src=${item.img} alt=${item.name}/>
+        <p><strong>${item.name}</strong> ${item.description}</p>
+      </a>
+    </div>`;
+});
+
+document.getElementById("react-projects").innerHTML = listOfProjects;
